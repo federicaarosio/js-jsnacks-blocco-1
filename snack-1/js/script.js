b92b715e86b3ec5b1dae8c58a0f4ef81b368e7c0
federicaarosio <arosio.fede@gmail.com>
2023-11-10 12:05:20 +0100

    added parseInt

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,13 +1,12 @@
 // chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
 
-const agePerson1 = prompt("Insert the age of person 1");
-const agePerson2 = prompt("Insert the age of person 2");
+const agePerson1 = paseInt(prompt("Type the age of person 1"));
+const agePerson2 = parseInt(prompt("Type the age of person 2"));
 
 if (agePerson1 > agePerson2) {
     document.getElementById("output").innerHTML = "The first person is older than the second";
 } else if (agePerson2 > agePerson1) {
     document.getElementById("output").innerHTML = "The second person is older than the first";
-}
-else {
-    document.getElementById("output").innerHTML = "Person 1 and person 2 have the same age";
+} else {
+    document.getElementById("output").innerHTML = "Person 1 and person 2 are of the same age";
 }
