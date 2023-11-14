25b459723fe70c3ac35061b45a7f7ac90968965c
federicaarosio <arosio.fede@gmail.com>
2023-11-14 12:17:34 +0100

    added console.log

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,5 +1,8 @@
 // Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
 
-let numberElement = document.querySelector("div.number");
+const numberElement = document.querySelector("div.number");
 
-for (let numberElement = 1; numberElement <= 6;  numberElement++)
+for (let numberElement = 1; numberElement <= 6;  numberElement++) {
+    numberElement.innerHTML += 1;
+    console.log(numberElement);
+}
