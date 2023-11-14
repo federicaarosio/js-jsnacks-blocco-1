5cb670e8d3db048254afe93c46c4f9d9e45cb852
federicaarosio <arosio.fede@gmail.com>
2023-11-14 12:11:59 +0100

    inizialized for loop

---
+++ snack-2/js/script.js
@@ -0,0 +1,5 @@
+// Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
+
+let numberElement = document.querySelector("div.number");
+
+for (let numberElement = 1; numberElement <= 6;  numberElement++)
