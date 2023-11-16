3a5a4af38479e980c936921782be7e15ce8254c0
federicaarosio <arosio.fede@gmail.com>
2023-11-16 17:53:51 +0100

    alternative without chartAt

---
+++ snack-10/js/script.js
@@ -0,0 +1,1 @@
+// Inserisci un <h1> al centro della pagina con un numero randomico all'interno e un bottone sotto. Ad ogni click del bottone aumento il numero nell'h1 di 3 e lo aggiorno in pagina.  
--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -9,26 +9,31 @@
 
 
 
+// const userNumber = prompt("Inserisci un numero di 7 cifre");
+
+// let somma = 0; 
+
+// for(i = 0 ; i < userNumber.length; i++) {
+//     // console.log(userNumber.charAt(i));
+//      // somma = somma + userNumber[i];
+//      somma += parseInt(userNumber.charAt(i));
+//      console.log("giro n:" + i + " - lettera analizzata:" + userNumber.charAt(i) + "- somma:" + somma);
+//     }
+
+// console.log(somma);
+
+
+
 const userNumber = prompt("Inserisci un numero di 7 cifre");
-
-//console.log(userNumber.charAt(0));
-
-// let userNumber = [1,2,3,4,5,6,7];
-
-// const firstNumber = (userNumber % 10);
-// console.log(firstNumber);
-
-// const secondNumber = ((userNumber % 100 - firstNumber) / 10);
-// console.log(secondNumber);
 
 let somma = 0; 
 
 for(i = 0 ; i < userNumber.length; i++) {
-    // console.log(userNumber.charAt(i));
-     // somma = somma + userNumber[i];
-     somma += parseInt(userNumber.charAt(i));
+    
+     somma += parseInt(userNumber[i]);
      console.log("giro n:" + i + " - lettera analizzata:" + userNumber.charAt(i) + "- somma:" + somma);
     }
 
 console.log(somma);
+
 
