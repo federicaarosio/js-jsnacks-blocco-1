febb4620066f5769d9a96e686ab14932c9a39478
federicaarosio <arosio.fede@gmail.com>
2023-11-16 17:00:25 +0100

    snack-8 completato

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -9,15 +9,26 @@
 
 
 
-const userNumber = parseInt(prompt("Inserisci un numero di 7 cifre"));
+const userNumber = prompt("Inserisci un numero di 7 cifre");
 
-const firstNumber = (userNumber % 10);
-console.log(firstNumber);
+//console.log(userNumber.charAt(0));
 
-const secondNumber = ((userNumber % 100 - firstNumber) / 10);
-console.log(secondNumber);
+// let userNumber = [1,2,3,4,5,6,7];
 
+// const firstNumber = (userNumber % 10);
+// console.log(firstNumber);
 
-// for(i = 0 ; i < 6; i++) {
-//     const sumDigits = 
-// }
+// const secondNumber = ((userNumber % 100 - firstNumber) / 10);
+// console.log(secondNumber);
+
+let somma = 0; 
+
+for(i = 0 ; i < userNumber.length; i++) {
+    // console.log(userNumber.charAt(i));
+     // somma = somma + userNumber[i];
+     somma += parseInt(userNumber.charAt(i));
+     console.log("giro n:" + i + " - lettera analizzata:" + userNumber.charAt(i) + "- somma:" + somma);
+    }
+
+console.log(somma);
+
